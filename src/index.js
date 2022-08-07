d1f1043be0d756d9950fa2b16ea146e7912ff16a
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-07 18:26:10 +0100

    react app is running

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-// create the root element to render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// render the react app within the root element
 root.render(<App />);
