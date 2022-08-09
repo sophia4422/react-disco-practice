ddca96da72d71ce967f815637b79e32098db5d49
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-09 12:34:01 +0100

    phone styling

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="monkey-grid">
+      <div className="output">
+        <div className="current-operand">2222222</div>
+      </div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>/</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>/</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>/</button>
+      <button>0</button>
+      <button className="span-two">Call</button>
+      <button>DEL</button>
+    </div>
+  );
+}
+
+export default App;
