da3b7fa497e104fee722a50d319ece87672281a7
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-09 13:03:09 +0100

    numbers show on the screen when pressed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import { useReducer } from "react";
+import DigitButton from "./DigitButton";
 import "./index.css";
 
-function reducer(state, action) {}
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  DELETE_DIGIT: "delete-digit",
+  CALL: "call",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+  }
+}
 
 function App() {
   const [{ currentOperand, operation }, dispatch] = useReducer(reducer, {});
+
   return (
     <div className="monkey-grid">
       <div className="output">
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>*</button>
-      <button>0</button>
-      <button>#</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <DigitButton digit="*" dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <DigitButton digit="#" dispatch={dispatch} />
       <button className="span-two">Call</button>
       <button>DEL</button>
     </div>
